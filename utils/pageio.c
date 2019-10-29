5fc86e778dd92848e18150cd1577b3a88adaa3b0
Bibaswan Khadka <f003dt8@babylon1.thayer.dartmouth.edu>
2019-10-29 16:55:39 -0400

    finished indexer

--- utils/pageio.c
+++ utils/pageio.c
@@ -7,12 +7,12 @@
 #include <hash.h>
 #include <webpage.h>
 
-int32_t pagesave(webpage_t *page,int id,char* dirnm){
+int32_t pagesave(webpage_t *pagep,int id,char* dirnm){
   char name[12];
   sprintf(name,"%d",id);
   char* slash="/";
-  char* dircopy = assertp(malloc(strlen(dirname)+strlen(slash)+strlen(name)+1), "dict copy");
-  strcpy(dircopy,dirname);
+  char* dircopy = assertp(malloc(strlen(dirnm)+strlen(slash)+strlen(name)+1), "dict copy");
+  strcpy(dircopy,dirnm);
   char *pathname=strcat(dircopy,slash);
   char *path=strcat(pathname,name);
   FILE* fp;
@@ -21,12 +21,16 @@ int32_t pagesave(webpage_t *page,int id,char* dirnm){
     printf("the file pointer is NULL");
     return 1;
   }
-  char *URL=webpage_getURL(page);
-  int depth=webpage_getDepth(page);
-  int htmllen=webpage_getHTMLlen(page);
-  char *HTML=webpage_getHTML(page);
+  char *URL=webpage_getURL(pagep);
+  int depth=webpage_getDepth(pagep);
+  int htmllen=webpage_getHTMLlen(pagep);
+  char *HTML=webpage_getHTML(pagep);
   fprintf(fp,"%s\n%d\n%d\n%s\n",URL,depth,htmllen,HTML);
   fclose(fp);
   free(dircopy);
   return 0;
 }
+
+webpage_t *pageload(int id, char *dirnm){
+  printf("hello");
+} 
