203f9cec8377724f9aff8c583ffd74de7d479b85
Jack Vasu <jvasu@studymaker.com>
2019-11-12 18:10:32 -0500

    changes to locked-hash.c

--- utils/locked-hash.c
+++ utils/locked-hash.c
@@ -1,5 +1,5 @@
 /* locked-hash.c --- locked hash
- * 
+1;5202;0c * 
  * 
  * Author: Samuel H. Baker
  * Created: Tue Nov  5 17:30:15 2019 (-0500)
@@ -20,38 +20,41 @@ pthread_mutex_t m;
 typedef struct lhash{
 	hashtable_t *h;
 
-} lhash_t;
+} lhash_s;
 
 lhash_t *lhopen(uint32_t lhsize){
 	pthread_mutex_init(&m,NULL);
-	lhash_t* lockedH= malloc(sizeof(lhash_t*));
+	lhash_s* lockedH= malloc(sizeof(lhash_s*));
 	if (lockedH == NULL) {
 		return NULL;
 	}
 	else{
-		hashtable_t* hold = malloc(sizeof(hashtable_t*));
-		hold = hopen(lhsize);
+		hashtable_t* hold = hopen(lhsize);
 		lockedH->h=hold;
-		return lockedH;
+		lhash_t* lockH = (lhash_t*)lockedH;
+		return lockH;
 	}
 }
 
 int32_t lhput(lhash_t *lhtp, void *ep, const char *key, int keylen){
 	pthread_mutex_lock(&m);
-	uint32_t returned=hput(lhtp->h,ep,key,keylen);
+	lhash_s* lockH = (lhash_s*)lhtp;
+	uint32_t returned=hput(lockH->h,ep,key,keylen);
 	pthread_mutex_unlock(&m);
 	return returned;
 }
 
 void lhapply(lhash_t *lhtp, void (*fn)(void* ep)){
 	pthread_mutex_lock(&m);
-	happly(lhtp->h,(fn));
+	lhash_s* lockedH = (lhash_s*)lhtp;
+	happly(lockedH->h,(fn));
 	pthread_mutex_unlock(&m);
 }
 
 void lhsearch(lhash_t *lhtp, bool (*searchfn)(void* elementp, const void* searchkeyp), const char *key, int32_t keylen){
 	pthread_mutex_lock(&m);
-	hsearch(lhtp->h,searchfn,key,keylen);
+	lhash_s* lockedH = (lhash_s*) lhtp;
+	hsearch(lockedH->h,searchfn,key,keylen);
 	pthread_mutex_unlock(&m);
 }
 
@@ -60,13 +63,16 @@ void lhremove(lhash_t* lhtp,
 							 const char *key,
 							 int32_t keylen) {
 	pthread_mutex_lock(&m);
-	hremove(lhtp->h,searchfn,key,keylen);
+	lhash_s* lockedH = (lhash_s*)lhtp;
+	hremove(lockedH->h,searchfn,key,keylen);
 	pthread_mutex_unlock(&m);
 }
 
 void lhclose(lhash_t* lhtp) {
 	pthread_mutex_lock(&m);
-	hclose(lhtp->h);
+	lhash_s* lockedH = (lhash_s*)lhtp;
+	hclose(lockedH->h);
+	free(lockedH);
 	pthread_mutex_unlock(&m);
 	pthread_mutex_destroy(&m);
 }
--- utils/locked-queue.c
+++ utils/locked-queue.c
@@ -8,12 +8,10 @@
  * Description: 
  * 
  */
-<<<<<<< HEAD
-#include <queue.h>
-=======
+
 #include "locked-queue.h"
 #include "queue.h"
->>>>>>> locked
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,39 +21,20 @@ pthread_mutex_t m;
 typedef struct lqueue{
 	queue_t *q;
 
-<<<<<<< HEAD
-} lqueue_t;
-
-lqueue_t* lqopen(void) {
-	pthred_mutex_init(&m,NULL);
-	lqueue_t* lockedQ= malloc(sizeof(lqueue_t*));
-=======
 } lqueue_s;
 
 lqueue_t* lqopen(void) {
 	pthread_mutex_init(&m,NULL);
 	lqueue_s* lockedQ= (lqueue_s*)malloc(sizeof(lqueue_s*));
->>>>>>> locked
 	if (lockedQ == NULL) {
 		return NULL;
 	}
 	else{
-<<<<<<< HEAD
-		queue_t* hold = malloc(sizeof(queue_t*));
-		hold = qopen();
+		lqueue_s* hold = qopen();
 		lockedQ->q=hold;
-		return lockedQ;
-	}
+return lockedQ;
 }
-void lqput(lqueue_t *lqp, void *elementp) {
-	pthread_mutex_lock(&m);
-	qput(lqp->q,elementp);
-=======
-		queue_t* hold = qopen();
-		lockedQ->q=hold;
-		lqueue_t* lq = (lqueue_t*)lockedQ;
-		return lq;
-	}
+}
 }
 int32_t lqput(lqueue_t *lqp, void *elementp) {
 	pthread_mutex_lock(&m);
@@ -68,56 +47,42 @@ int32_t lqput(lqueue_t *lqp, void *elementp) {
 		toReturn=0;
 	}
 	return toReturn;
->>>>>>> locked
 	pthread_mutex_unlock(&m);
 }
 
 void* lqget(lqueue_t *lqp) {
 	pthread_mutex_lock(&m);
-<<<<<<< HEAD
 	void* item = qget(lqp->q);
 	pthread_mutex_unlock(&m);
 	return item;
-=======
 	lqueue_s* lqueue = (lqueue_s*)lqp;
 	return qget(lqueue->q);
 	pthread_mutex_unlock(&m);
->>>>>>> locked
 }
 
 void lqapply(lqueue_t *lqp, void (*fn)(void* elementp)) {
 	pthread_mutex_lock(&m);
-<<<<<<< HEAD
  	qapply(lqp->q,(fn));
-=======
 	lqueue_s* lqueue = (lqueue_s*)lqp;
 	qapply(lqueue->q,(fn));
->>>>>>> locked
 	pthread_mutex_unlock(&m);
 }
 
 void* lqsearch(lqueue_t *lqp, bool (*searchfn)(void* elementp,const void* keyp),const void* skeyp) {
 	pthread_mutex_lock(&m);
-<<<<<<< HEAD
 	void* search = qsearch(lqp->q, searchfn,skeyp);
-=======
 	lqueue_s* lqueue = (lqueue_s*)lqp;
 	void* search = qsearch(lqueue->q, searchfn,skeyp);
->>>>>>> locked
 	pthread_mutex_unlock(&m);
 	return search;
 }
 
 void lqclose(lqueue_t *lqp) {
 	pthread_mutex_lock(&m);
-<<<<<<< HEAD
-	qclose(lqp->q);
 	pthread_mutex_unlock(&m);
-=======
 	lqueue_s* lqueue = (lqueue_s*)lqp;
 	qclose(lqueue->q);
 	free(lqueue);
  	pthread_mutex_unlock(&m);
->>>>>>> locked
 	pthread_mutex_destroy(&m);
 }
